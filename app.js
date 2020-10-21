7e13d4ed8c32df20ec37e3a63bd3cc1a6ea13cbb
Datta <yayathi2000@gmail.com>
2020-10-21 23:19:04 +0530

    all done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const morgan = require('morgan')
+// const morgan = require('morgan')
 
 mongoose.connect("mongodb+srv://glitch:GlItCh@cluster0.tbjsd.mongodb.net/GROWTHSOURCE",{
     useNewUrlParser: true,
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 })
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 
 const loanRoutes = require('./src/routes/loanApplication')
 
